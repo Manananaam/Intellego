e378f5cb5604eebac94bc37752f96d65b46ba107
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-01-26 15:45:26 -0500

    feat: added User db model

---
+++ server/db/models/userModel.js
@@ -0,0 +1,31 @@
+const db = require("../db");
+const Sequelize = require("sequelize");
+
+const User = db.define("user", {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: Sequelize.ENUM,
+    values: ["teacher"],
+  },
+});
+
+module.exports = User;
