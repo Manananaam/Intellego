13037b02dd1768e640b513b5a340dd2527e06ea8
Na Lin <lna483018@gmail.com>
2023-01-27 22:32:08 -0500

    add studentRouter to api

--- server/api/index.js
+++ server/api/index.js
@@ -8,5 +8,6 @@ router.use((req, res, next) => {
 
 // Sub-router, start with /api
 router.use("/template", require("./templateRouter"));
+router.use("/students", require("./studentRouter"));
 
 module.exports = router;
