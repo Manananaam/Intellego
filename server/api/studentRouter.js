c77e2e059fdda5ebc0ee28e46da6504f25c6a761
Na Lin <lna483018@gmail.com>
2023-01-27 23:11:26 -0500

    create student Router

---
+++ server/api/studentRouter.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const asyncHandler = require("express-async-handler");
+const AppError = require("../utils/appError");
+const {
+  models: { Student, Course, Course_Student },
+} = require("../db");
+
+module.exports = router;
