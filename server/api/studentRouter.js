69876fd5455e4d9641e545d11a906b455ef753d1
Na Lin <lna483018@gmail.com>
2023-01-27 23:12:47 -0500

    feat: get individual student's enrollments

--- server/api/studentRouter.js
+++ server/api/studentRouter.js
@@ -7,4 +7,17 @@ const {
   models: { Student, Course, Course_Student },
 } = require("../db");
 
+// @desc: get the student and a list of courses that student enrolled
+// @route: GET /api/students/:studentId
+// @access: public
+router.get(
+  "/:studentId",
+  asyncHandler(async (req, res, next) => {
+    const students = await Student.findByPk(req.params.studentId, {
+      include: [{ model: Course }],
+    });
+    res.send(students);
+  })
+);
+
 module.exports = router;
