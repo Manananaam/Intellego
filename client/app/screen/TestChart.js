f837f6676651bc43d301848659e55fe403bbdd41
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-01-30 13:03:39 -0500

    chore: completed test proof of concept to generate pie chart from data

---
+++ client/app/screen/TestChart.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const testData = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45677,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 78888,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 4300,
+    userLost: 234,
+  },
+];
+export const data = {
+  labels: ["2016", "2017", "2018", "2019", "2020"],
+  datasets: [
+    {
+      label: "Test chart",
+      data: testData.map((year) => year.userGain),
+
+      backgroundColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+      ],
+      borderColor: "#000000",
+
+      borderWidth: 1,
+    },
+  ],
+};
+export function SamplePieChart() {
+  return <Pie data={data} />;
+}
