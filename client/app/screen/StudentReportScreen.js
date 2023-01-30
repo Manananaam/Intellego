c3fd074af3b1fa144895b5a05750068e8f535285
Na Lin <lna483018@gmail.com>
2023-01-30 17:24:11 -0500

    render student's grades in each assessment with dummy data rendered with bar chart format

--- client/app/screen/StudentReportScreen.js
+++ client/app/screen/StudentReportScreen.js
@@ -1,5 +1,57 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+
+// Chart
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  Tooltip,
+  Legend,
+  LinearScale,
+  BarElement,
+} from "chart.js";
+
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, Tooltip, Legend, BarElement);
+
+const response_data = [
+  {
+    id: 1,
+    title: "Test Assessment 1 ",
+    total_grade: 1,
+  },
+  {
+    id: 5,
+    title: "Test Assessment 2",
+    total_grade: 3,
+  },
+];
 
 export default function StudentReportScreen() {
-  return <div>student report screen</div>;
+  const data = {
+    labels: response_data.map((el) => el.title),
+
+    datasets: [
+      {
+        label: "Test chart",
+        data: response_data.map((el) => el.total_grade),
+
+        backgroundColor: "aqua",
+        borderColor: "#000",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {};
+
+  return (
+    <div>
+      <p className="">student name</p>
+      <div style={{ width: "50%" }}>
+        <Bar data={data} options={options}></Bar>
+      </div>
+    </div>
+  );
 }
