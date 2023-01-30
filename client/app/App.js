c12c839701bbe3567fea639c3b0e478a9832faf3
Na Lin <lna483018@gmail.com>
2023-01-30 16:31:37 -0500

    screen: initialize student report screen and render in page under /reports/students path

--- client/app/App.js
+++ client/app/App.js
@@ -4,12 +4,16 @@ import { SamplePieChart } from "./screen/TestChart";
 // Router
 import { Routes, Route } from "react-router-dom";
 
+// Screen
+import StudentReportScreen from "./screen/StudentReportScreen";
+
 export default function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<h1>That's start our Capstone project!</h1>} />
-        <Route path='/test' element={<SamplePieChart />} />
+        <Route path="/" element={<h1>That's start our Capstone project!</h1>} />
+        <Route path="/test" element={<SamplePieChart />} />
+        <Route path="/reports/students" element={<StudentReportScreen />} />
       </Routes>
     </div>
   );
---
+++ client/app/screen/StudentReportScreen.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function StudentReportScreen() {
+  return <div>student report screen</div>;
+}
