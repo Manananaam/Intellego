00b352a70214d550569fb6b63ae174b900410779
Na Lin <lna483018@gmail.com>
2023-01-30 20:22:25 -0500

    add import to make bootstrap work

--- client/index.js
+++ client/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "./app/store";
 import App from "./app/App";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import "bootstrap/dist/css/bootstrap.css";
 const root = createRoot(document.getElementById("app"));
 
 root.render(
