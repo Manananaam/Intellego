f4fb6d1ba4dacde0541f2f8964d779821b2ea8f4
Na Lin <lna483018@gmail.com>
2023-01-30 20:54:10 -0500

    fix api route error and export fetch method

--- client/app/store/slices/studentReportSlice.js
+++ client/app/store/slices/studentReportSlice.js
@@ -2,12 +2,12 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 //fetch individual student's grade for each assessment
-const fetchGradeForEachAssessment = createAsyncThunk(
+export const fetchGradeForEachAssessment = createAsyncThunk(
   "report/student/fetchGradeForEachAssessment",
   async ({ studentId, courseId }, { rejectWithValue }) => {
     try {
       const { data } = await axios.get(
-        `/${studentId}/courses/${courseId}/assessments`
+        `/api/students/${studentId}/courses/${courseId}/assessments`
       );
       return data;
     } catch (err) {
@@ -19,11 +19,13 @@ const fetchGradeForEachAssessment = createAsyncThunk(
 const studentReportSlice = createSlice({
   name: "template",
   initialState: {
+    student: null,
     grades: [],
   },
   reducers: {},
   extraReducers(builder) {
     builder.addCase(fetchGradeForEachAssessment.fulfilled, (state, action) => {
+      state.student = action.payload.student;
       state.grades = action.payload.listOfGrade;
     });
   },
