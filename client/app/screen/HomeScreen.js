44b6276b9eccb52b29ba3a8c1aa9781217c4fb45
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-01-30 23:21:47 -0500

    feat: implement log out button

--- client/app/screen/HomeScreen.js
+++ client/app/screen/HomeScreen.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { selectAuthState } from "../store/slices/authSlice";
+import { selectAuthState, logout } from "../store/slices/authSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
   const { isLoading, user } = useSelector(selectAuthState);
+  const handleLogout = () => {
+    dispatch(logout());
+  };
   if (user) {
     return (
       <>
         <p>
           hello, {user.firstName} {user.lastName}!
         </p>
+        <button onClick={handleLogout}>log out</button>
       </>
     );
   }
--- client/app/screen/SignUpScreen.js
+++ client/app/screen/SignUpScreen.js
@@ -88,37 +88,6 @@ const SignUpScreen = () => {
         />
         <button type='submit'>Submit</button>
       </form>
-      {/* <Form>
-        <div>
-          <Form.Group className='mb-3' controlId='formBasicFirstName'>
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type='text' placeholder='Enter first name' />
-          </Form.Group>
-        </div>
-        <div>
-          <Form.Group className='mb-3' controlId='formBasicLastName'>
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control type='text' placeholder='Enter last name' />
-          </Form.Group>
-        </div>
-        <div>
-          <Form.Group className='mb-3' controlId='formBasicEmail'>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type='email' placeholder='Enter email' />
-          </Form.Group>
-        </div>
-        <div>
-          <Form.Group className='mb-3' controlId='formBasicPassword'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control type='password' placeholder='Password' />
-          </Form.Group>
-        </div>
-        <div>
-          <Button variant='primary' type='submit'>
-            Submit
-          </Button>
-        </div>
-      </Form> */}
     </>
   );
 };
