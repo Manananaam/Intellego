b325becdeb657492432c3b0d0d62dd3e16a6e23a
Na Lin <lna483018@gmail.com>
2023-01-30 21:05:51 -0500

    show dropdown menu with list of course, and show list of student based on the course

--- client/app/screen/StudentReportScreen.js
+++ client/app/screen/StudentReportScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 //Bootstrap
 import Dropdown from "react-bootstrap/Dropdown";
@@ -21,21 +21,44 @@ import { Bar } from "react-chartjs-2";
 
 ChartJS.register(CategoryScale, LinearScale, Tooltip, Legend, BarElement);
 
+// dummy data for a list of courses that teacher is in charge of
+const courses = [
+  {
+    id: 14,
+    name: "Test Course",
+    subject: "ELA",
+    gradeLevel: 3,
+    createdAt: "2023-01-30T17:26:32.945Z",
+    updatedAt: "2023-01-30T17:26:32.945Z",
+    userId: 1,
+    assessmentId: null,
+  },
+];
+
 export default function StudentReportScreen() {
   // fetch a list of student belongs to the course
   const dispatch = useDispatch();
+  const [currentCourse, setCurrentCourse] = useState(null);
   const { students } = useSelector((state) => state.studentEnroll);
   const { grades, student } = useSelector((state) => state.studentReport);
 
   useEffect(() => {
-    dispatch(fetchStudentList({ courseId: 20 }));
-  }, []);
+    if (currentCourse) {
+      dispatch(fetchStudentList({ courseId: currentCourse }));
+    }
+  }, [currentCourse]);
 
   const handleStudentGradeReport = (student) => {
-    console.log(student);
     dispatch(
-      fetchGradeForEachAssessment({ studentId: student.id, courseId: 20 })
+      fetchGradeForEachAssessment({
+        studentId: student.id,
+        courseId: currentCourse,
+      })
     );
+  };
+
+  const handleCurrentCourse = (course) => {
+    setCurrentCourse(course.id);
   };
 
   console.log(student, grades);
@@ -57,6 +80,21 @@ export default function StudentReportScreen() {
 
   return (
     <div>
+      <Dropdown>
+        <Dropdown.Toggle>Course</Dropdown.Toggle>
+        <Dropdown.Menu>
+          {courses.map((course) => {
+            return (
+              <Dropdown.Item
+                key={course.id}
+                onClick={() => handleCurrentCourse(course)}
+              >
+                {course.name}
+              </Dropdown.Item>
+            );
+          })}
+        </Dropdown.Menu>
+      </Dropdown>
       <Dropdown>
         <Dropdown.Toggle>Students</Dropdown.Toggle>
         <Dropdown.Menu>
