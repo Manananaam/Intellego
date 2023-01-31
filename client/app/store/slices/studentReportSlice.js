8927a29ed881d47e98186f4c3ec232ed8a7d7c6f
Na Lin <lna483018@gmail.com>
2023-01-30 19:48:21 -0500

    thunk: fetch individual student's grade for each assessment

--- client/app/store/slices/studentReportSlice.js
+++ client/app/store/slices/studentReportSlice.js
@@ -1,10 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//fetch individual student's grade for each assessment
+const fetchGradeForEachAssessment = createAsyncThunk(
+  "report/student/fetchGradeForEachAssessment",
+  async ({ studentId, courseId }, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(
+        `/${studentId}/courses/${courseId}/assessments`
+      );
+      return data;
+    } catch (err) {
+      return rejectWithValue(err.message);
+    }
+  }
+);
 
 const studentReportSlice = createSlice({
   name: "template",
-  initialState: {},
+  initialState: {
+    grades: [],
+  },
   reducers: {},
-  extraReducers(builder) {},
+  extraReducers(builder) {
+    builder.addCase(fetchGradeForEachAssessment.fulfilled, (state, action) => {
+      state.grades = action.payload.listOfGrade;
+    });
+  },
 });
 
 export const studentReportReducer = studentReportSlice.reducer;
