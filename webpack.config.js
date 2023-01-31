61cd7fe202329e724c706a94dd2a145876c10ae7
Na Lin <lna483018@gmail.com>
2023-01-30 20:14:32 -0500

    add webpack rule to make react-bootstrap work correctly

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
 module.exports = {
-  entry: ['./client/index.js'],
+  entry: ["./client/index.js"],
   output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js',
+    path: __dirname + "/public",
+    filename: "bundle.js",
   },
   context: __dirname,
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: ["@babel/preset-env", "@babel/preset-react"],
         },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
