c79cc0853309862d8237b10c65e0195ad95a561d
Na Lin <lna483018@gmail.com>
2023-01-31 21:12:02 -0500

    add current student data in slice

--- client/app/store/slices/studentReportSlice.js
+++ client/app/store/slices/studentReportSlice.js
@@ -19,6 +19,7 @@ export const fetchGradeForEachAssessment = createAsyncThunk(
 const studentReportSlice = createSlice({
   name: "template",
   initialState: {
+    student: null,
     grades: [],
   },
   reducers: {},
