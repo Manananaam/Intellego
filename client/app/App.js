c4c10bfbe5b90ae516d46322ed5ef5fc0ae2ef37
Na Lin <lna483018@gmail.com>
2023-02-01 14:54:01 -0500

    create student view screen component and add route path to render it

--- client/app/App.js
+++ client/app/App.js
@@ -5,6 +5,7 @@ import LogInScreen from "./screen/LogInScreen";
 import SignUpScreen from "./screen/SignUpScreen";
 import AssessmentsScreen from "./screen/AssessmentsScreen";
 import EditAssessmentScreen from "./screen/EditAssessmentScreen";
+import StudentViewScreen from "./screen/StudentViewScreen";
 
 // Router
 import { Routes, Route } from "react-router-dom";
@@ -15,15 +16,23 @@ export default function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<HomeScreen />} />
+        <Route path="/" element={<HomeScreen />} />
         <Route path="/courses" element={<CousreScreen />} />
         <Route
           path="/courses/:courseId/students"
-          element={<CourseStudentScreen />} />
-        <Route path='/login' element={<LogInScreen />} />
-        <Route path='/signup' element={<SignUpScreen />} />
+          element={<CourseStudentScreen />}
+        />
+        <Route path="/login" element={<LogInScreen />} />
+        <Route path="/signup" element={<SignUpScreen />} />
         <Route path="/assessments" element={<AssessmentsScreen />} />
-        <Route path="/assessments/:assessmentId" element={<EditAssessmentScreen />} />
+        <Route
+          path="/assessments/:assessmentId"
+          element={<EditAssessmentScreen />}
+        />
+        <Route
+          path="/student/assessment/:assessmentId"
+          element={<StudentViewScreen />}
+        />
       </Routes>
     </div>
   );
---
+++ client/app/screen/StudentViewScreen.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function StudentViewScreen() {
+  return <div>student view screen</div>;
+}
