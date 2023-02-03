65fadaf65de99124efd2af53ba9b6704d9388647
ndudar <ndudar@gmail.com>
2023-02-03 13:40:58 -0600

    feat: initial rendering of drop downs no data

--- client/app/screen/AssessmentReportScreen.js
+++ client/app/screen/AssessmentReportScreen.js
@@ -1,10 +1,38 @@
 import React from "react";
+
+//Bootstrap
 import Table from "react-bootstrap/Table";
+import Dropdown from "react-bootstrap/Dropdown";
+
+//redux
+import { getCourses } from "../store";
 
 const AssessmentReportScreen = () => {
+  // redux state
+  // fetch a list of courses managed by current user
+  const { allcourses } = useSelector((state) => state.studentEnroll);
+
   return (
     <>
       <h1>Assessment Report Screen</h1>
+      <Dropdown>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          Choose Course
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item>Sample Course</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <br />
+      <Dropdown>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          Choose Assessment
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item>Sample Assessment</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <br />
       <h2>Assessment Title</h2>
       <Table striped bordered hover>
         <thead>
