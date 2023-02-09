fa7899bb912d5d9dd48754437937786f43d0bc0c
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-02-09 15:38:56 -0500

    fix: updated sign in page validation set up so that it performs correct form validation

--- client/app/screen/SignUpScreen.js
+++ client/app/screen/SignUpScreen.js
@@ -1,89 +1,79 @@
 import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Button, Form } from "react-bootstrap";
-import { useFormik } from "formik";
-import * as yup from "yup";
+import { Button } from "react-bootstrap";
+import { useFormik, Formik, Field, Form } from "formik";
+import * as Yup from "yup";
 import { useDispatch, useSelector } from "react-redux";
 import { selectAuthState, signup } from "../store/slices/authSlice";
-
-const validate = yup.object({
-  email: yup
-    .string("Enter your email")
-    .email("Enter a valid email")
-    .required("Email is required"),
-  password: yup
-    .string("Enter your password")
-    .min(6, "Please enter a password that is 6 characters or more")
-    .required("Password is required"),
-  firstName: yup
-    .string("Enter your first name")
-    .required("First name is a required field"),
-  lastName: yup
-    .string("Enter your first name")
-    .required("Last name is a required field"),
-});
 
 const SignUpScreen = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { isLoading, user } = useSelector(selectAuthState);
 
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-      firstName: "",
-      lastName: "",
-    },
-    validationSchema: validate,
-    onSubmit: (values) => {
-      console.log(
-        "howdy, from formik handlesubmit, here are the values",
-        values
-      );
-      dispatch(signup(values));
-      navigate("/");
-    },
+  const SignupValidate = Yup.object().shape({
+    email: Yup.string("Enter your email")
+      .email("Enter a valid email")
+      .required("Email is required"),
+    password: Yup.string("Enter your password")
+      .min(6, "Please enter a password that is 6 characters or more")
+      .required("Password is required"),
+    firstName: Yup.string("Enter your first name").required(
+      "First name is a required field"
+    ),
+    lastName: Yup.string("Enter your last name").required(
+      "Last name is a required field"
+    ),
   });
 
   return (
     <>
-      <p>hi, this will be the signup screen</p>
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="firstName">First Name</label>
-        <input
-          id="firstName"
-          name="firstName"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.firstName}
-        />
-        <label htmlFor="lastName">Last Name</label>
-        <input
-          id="lastName"
-          name="lastName"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.lastName}
-        />
-        <label htmlFor="email">Email Address</label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          name="password"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <h1>Sign up for Intellego!</h1>
+      <Formik
+        initialValues={{ firstName: "", lastName: "", email: "", password: "" }}
+        validationSchema={SignupValidate}
+        onSubmit={(values) => {
+          console.log("formik handlesubmit");
+          console.log("values", values);
+          dispatch(signup(values));
+          navigate("/");
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <>
+              <label htmlFor='firstName'>First Name</label>
+              <Field name='firstName' />
+              {errors.firstName && touched.firstName ? (
+                <div>{errors.firstName}</div>
+              ) : null}
+            </>
+            <br />
+            <>
+              <label htmlFor='lastName'>Last Name</label>
+              <Field name='lastName' />
+              {errors.lastName && touched.lastName ? (
+                <div>{errors.lastName}</div>
+              ) : null}
+            </>
+            <br />
+            <>
+              <label htmlFor='email'>Email Address</label>
+              <Field name='email' type='email' />
+              {errors.email && touched.email ? <div>{errors.email}</div> : null}
+            </>
+            <br />
+            <>
+              <label htmlFor='password'>Password</label>
+              <Field name='password' type='password' />
+              {errors.password && touched.password ? (
+                <div>{errors.password}</div>
+              ) : null}
+            </>
+            <button type='submit'>Submit</button>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 };
