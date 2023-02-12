af28aef96a82172a1d2ad9aadef69242c8ac1bd6
Amy Chun <amychun@msn.com>
2023-02-12 14:04:05 -0800

    working on dashboard

--- client/app/screen/DashHome.js
+++ client/app/screen/DashHome.js
@@ -1,35 +1,39 @@
 //react
 import React from "react";
-
+import { Link } from "react-router-dom";
 //styling
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import "../components/style/layout.css";
+import CourseCreate from "../components/CourseCreate";
 
 const DashHome = () => {
   return (
     <Container>
       <Row>
         <h1>Quick Actions</h1>
-        <Col xs={6} md={4}>
+        {/* <Col xs={6} md={4}>
           <Card id="studentCard_dash">
             <Card.Body>
               <Card.Title>Add Student</Card.Title>
               <Card.Text id="cardText">+</Card.Text>
             </Card.Body>
           </Card>
-        </Col>
-        <Col xs={6} md={4}>
+        </Col> */}
+        <Col xs={9} md={6}>
           <Card id="courseCard_dash">
             <Card.Body>
               <Card.Title>Add Course</Card.Title>
               <Card.Text id="cardText">+</Card.Text>
+              <Link to="/courses">
+                <CourseCreate />
+              </Link>
             </Card.Body>
           </Card>
         </Col>
-        <Col xs={6} md={4}>
+        <Col xs={9} md={6}>
           <Card id="assessmentCard_dash">
             <Card.Body>
               <Card.Title>Add Assessment</Card.Title>
